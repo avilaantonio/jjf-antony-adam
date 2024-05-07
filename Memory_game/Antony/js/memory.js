555d7e357a24cad25a612524f168d043fc6be880
Antony <163296963+avilaantonio@users.noreply.github.com>
2024-05-07 14:51:42 +0200

    Solve: show 4 random guitar

--- Memory_game/Antony/js/memory.js
+++ Memory_game/Antony/js/memory.js
@@ -1,18 +1,21 @@
+const imgHolderDiv = document.getElementById('imgHolderDiv');
 function startGame() {
     let numberOfCards = getNumberOfCards();
     let randomPictures = generateRandomPicArrangement(numberOfCards);
+
     generatedCards(numberOfCards, randomPictures);
 }
 
+const images = ['img/01a.jpg', 'img/01b.jpg', 'img/02a.jpg', 'img/02b.jpg'];
 function generateRandomPicArrangement(numberOfCards) {
     let pictures = [];
     do {
-        let randomNumber = Math.floor(Math.random() * (numberOfCards)) + 1;
-        if (!pictures.includes(randomNumber)) {
-            pictures.push(randomNumber)
+        let randomNumber = Math.floor(Math.random() * (numberOfCards));
+        if (!pictures.includes(images[randomNumber])) {
+            pictures.push(images[randomNumber])
         }
     } while (pictures.length != numberOfCards);
-    return pictures;
+    return pictures;   
 }
 
 function getNumberOfCards() {
@@ -30,53 +33,26 @@ function getNumberOfCards() {
     document.getElementById("startButton").disabled = true;
     return numberOfCards;
 }
-// 37. 
-let images = ['img/01a.jpg', 'img/01b.jpg', 'img/02a.jpg', 'img/02b.jpg'];
 
 function generatedCards(numberOfCards, randomPictures) {
-    let randomPic = [];
-    do {
-        let randomNumber = Math.floor(Math.random() * (numberOfCards)) + 1;
-        if (!randomPic.includes(randomNumber)) {
-            randomPic.push(randomNumber)
-        }
-    } while (randomPic.length != numberOfCards);
-    console.log(randomPic);
-
+    imgHolderDiv.innerHTML = "";
     for (let i = 0; i < numberOfCards; i++) {
-
-        let div = document.createElement('div');
-        div.classList.add('col');
-        div.classList.add('mb-3');
-
-        /*
-        function randomImg(numbers, img) {
-            img = [];
-            for (let i = 0; i < numbers.length; i++) {
-                img.push("img/img" + numbers[i] + ".jpg");
-            }
-            return img;
-        }
-        */
-
-        let div2 = document.createElement('div');
-        div2.classList.add('square');
-        div2.classList.add('rounded');
-        div2.classList.add('d-none');
-        
-
-
-
-
-        div2.textContent = `p${randomPic[i]}`;
-        div.appendChild(div2);
-
-        document.querySelector('.row').appendChild(div);
+        //-------------- 2. -----------------
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = [
+            `<div class="col mb-3">`,
+            `   <div class="rounded">`,
+            `       <img id="${randomPictures[i]}" class="w-100" src="${randomPictures[i]}" alt="${randomPictures[i]}">`,
+            '   </div>',
+            '</div>'
+        ].join('');
+        imgHolderDiv.append(wrapper);
     }
 }
 
-//------------------------------------
-
+//------------- next ---------------
+// Select a card
+/*
 document.getElementsByClassName("selectCard").addEventListener("click", showImage);
 
 let counter = 0;
@@ -91,5 +67,6 @@ function showImage() {
         counter = 0;
     }
 }
-
-
+    
+}
+*/
