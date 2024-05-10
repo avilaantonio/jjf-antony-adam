52529e155e701fb91f6b34dc331797db92ec28bb
Antony <163296963+avilaantonio@users.noreply.github.com>
2024-05-10 13:32:13 +0200

    flipping-cards

---
+++ Memory_game/flipping_cards/script.js
@@ -0,0 +1,17 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    const divs = document.querySelectorAll(".square-div");
+    divs.forEach((div) => {
+        div.addEventListener("click", function () {
+            const img = div.querySelector("img");
+            if (img) {
+                if (img.style.display === "none") {
+                    img.style.display = "flex";
+                }
+                else {
+                    img.style.display = "none";
+                }
+            }
+        });
+    });
+});
